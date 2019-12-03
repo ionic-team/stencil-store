3f985ad3efc61067ce433910f66c72dad300ee96
Sergio Arbeo <sarbeo@apple.com>
2019-12-03 01:49:01 +0100

    Not subscribe at all if we are not in a stencil project

--- packages/stencil-store/src/subscriptions/stencil.ts
+++ packages/stencil-store/src/subscriptions/stencil.ts
@@ -7,9 +7,15 @@ import { appendToMap } from '../utils';
 export const stencilSubscription = <T>({ subscribe }: Pick<CreateStoreReturn<T>, 'subscribe'>) => {
   const elmsToUpdate = new Map<string, any[]>();
 
+  if (typeof getRenderingElement !== 'function') {
+    // If we are not in a stencil project, we do nothing.
+    // This function is not really exported by @stencil/core.
+    return;
+  }
+
   subscribe({
     get(_state, propName) {
-      const elm = typeof getRenderingElement === 'function' && getRenderingElement();
+      const elm = getRenderingElement();
       if (elm) {
         appendToMap(elmsToUpdate, propName as string, elm);
       }
