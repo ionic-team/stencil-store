6f46a040bd2887911f7cf46d59705eca790ce496
Sergio Arbeo <serabe@gmail.com>
2020-01-30 16:51:25 +0100

    Export types (#6)

--- packages/stencil-store/src/index.ts
+++ packages/stencil-store/src/index.ts
@@ -2,3 +2,7 @@ export { createStore } from './store';
 export { createObservableMap } from './observable-map';
 export { computedSubscription } from './subscriptions/computed';
 export { stencilSubscription } from './subscriptions/stencil';
+
+// Types
+
+export { ComputedReturn, ObservableMap, StoreSubscriptionObject, StoreSubscription } from './types';
