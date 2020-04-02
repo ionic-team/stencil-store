4ca0129ea614f29e6d9b036957171eb9cd1336f6
Manu MA <manu.mtza@gmail.com>
2020-04-02 18:57:08 +0200

    chore(): reorganize repo (#10)

--- test-app/stencil.config.ts
+++ test-app/stencil.config.ts
@@ -8,9 +8,6 @@ export const config: Config = {
       esmLoaderPath: '../loader'
     },
     {
-      type: 'docs-readme'
-    },
-    {
       type: 'www',
       serviceWorker: null // disable service workers
     }
