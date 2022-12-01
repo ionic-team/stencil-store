aebc87ef2a676cf3582f97c7da641f2e75416858
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-12-01 16:12:15 -0500

    chore(config): update import of package.json
    the package.json file can no longer be imported directly. since we are
    using node 16 (per the volta key in package.json), we follow the advice
    given https://rollupjs.org/guide/en/#importing-packagejson

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,7 @@
-import pkg from './package.json';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+
+const pkg = require('./package.json');
 
 export default {
   input: 'build/index.js',
