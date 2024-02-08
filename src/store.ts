8cbf7adedb241854b43aed7dc2dbf8d64b3590a7
Thien Huynh <thien@zonos.com>
2024-02-08 14:57:35 -0700

    refactor: adjust types to match with readme

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import { createObservableMap } from './observable-map';
 import { ObservableMap } from './types';
 
 export const createStore = <T extends { [key: string]: any }>(
-  defaultState?: T,
+  defaultState?: T | (() => T),
   shouldUpdate?: (newV: any, oldValue, prop: keyof T) => boolean,
 ): ObservableMap<T> => {
   const map = createObservableMap(defaultState, shouldUpdate);
